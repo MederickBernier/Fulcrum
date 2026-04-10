1f01ee070da8506b55a4284a32309dc7a14d228b
Mederick Bernier <mederick.bernier@hotmail.ca>
2026-04-10 01:29:56 -0400

    Added twig for templating and it's showing for the moment

--- public/index.php
+++ public/index.php
@@ -6,8 +6,18 @@ require_once dirname(__DIR__).'/vendor/autoload.php';
 
 use FastRoute\Dispatcher;
 use FastRoute\RouteCollector;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 use function FastRoute\simpleDispatcher;
+
+//Boot Twig
+$loader = new FilesystemLoader(dirname(__DIR__).'/resources/templates');
+$twig = new Environment($loader,[
+    'cache'       => false,
+    'debug'       => true,
+    'auto_reload' =>  true
+]);
 
 $dispatcher = simpleDispatcher(function(RouteCollector $r){
     $r->get('/', 'home');
@@ -27,19 +37,19 @@ $uri = rawurldecode($uri);
 $routeInfo = $dispatcher->dispatch($method, $uri);
 
 match($routeInfo[0]){
-    Dispatcher::NOT_FOUND => (function(){
+    Dispatcher::NOT_FOUND => (function() use ($twig){
         http_response_code(404);
-        echo '404 - Not Found';
+        echo $twig->render('errors/404.html.twig');
     })(),
-    Dispatcher::METHOD_NOT_ALLOWED => (function(){
+    Dispatcher::METHOD_NOT_ALLOWED => (function() use ($twig){
         http_response_code(405);
         echo '405 - Method not allowed';
     })(),
-    Dispatcher::FOUND => (function() use ($routeInfo){
+    Dispatcher::FOUND => (function() use ($routeInfo, $twig){
         echo match($routeInfo[1]){
-            'home' => 'Fulcrum is alive',
-            'admin.dashboard' => 'Admin panel coming soon',
-            default => '404 - Not Found',
+            'home'              => $twig->render('home.html.twig'),
+            'admin.dashboard'   => $twig->render('admin/dashboard/index.html.twig'),
+            default             => $twig->render('errors/404.html.twig'),
         };
     })(),
 };
